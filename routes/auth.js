e33d6879177710cb50c311d2187f6471f1414e24
SUNDARAVADIVU R <suganyajap@gmail.com>
2022-03-26 13:06:21 +0530

    small changes

--- routes/auth.js
+++ routes/auth.js
@@ -23,7 +23,7 @@ router.post("/register", async (req, res) => {
 //LOGIN
 router.post("/login", async (req, res) => {
   try {
-    const user = await User.findOne({ email: req.body.email});
+    const user = await User.findOne({username: req.body.username});
     !user && res.status(400).json("Wrong credentials!");
 
     const validated = await bcrypt.compare(req.body.password, user.password);
